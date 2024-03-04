791ea02c17d25049f4a2bf94faa002f22cb85fd9
dariusgatsby <dariusid08@gmail.com>
2024-03-03 22:29:04 -0800

    fixed deletion of improper folder

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import shutil
 from time import strftime, localtime
@@ -6,44 +8,60 @@ bold = "\033[1m"
 reset = "\033[0m"
 red = "\033[91m"
 os_name = os.uname()[0]
-folder_dir = os.path.expanduser("~/downloads.bak/")
+folder_dir = os.path.expanduser("~/Downloads/")
 folder_contents = os.listdir(folder_dir)
-#    shutil.rmtree("~/downloads.bak")
+home_dir = os.path.join(os.path.expanduser("~/"))
 
+# Starting display
 print(
     f"This will delete all downloads in {bold + red + os.uname()[1] + reset}")
 print(f"OS: {os_name}")
 
+# Get user confirmation
 user_input = input("Continue (y/n)? ").lower()
+
 deletion_mode = False
 if user_input == 'y':
     deletion_mode = True
 elif user_input == 'n':
     print("Best not to rush")
+
 while deletion_mode:
     choice = int(input(
-        "1. List files in the directory/folder\n2. Proceed to delete\n"))
+        "1. List files in the directory/folder\n2. Proceed to delete\n3.Exit\n"))
+
     if choice == 1:
         for file in folder_contents:
             path = f"{folder_dir}/{file}"
             metadata = strftime('%d-%m-%Y %H:%M:%S',
                                 localtime(os.path.getctime(path)))
-            print(f"Filename {file} | Last modification: {metadata}")
+            print(
+                f"Filename {bold + red +file + reset} | Last modification: {metadata}")
         continue
+
     elif choice == 2:
-        dummy_dir = "tbd"
-        tbd = os.path.join(os.path.expanduser("~/"), dummy_dir)
-        os.mkdir(tbd)
-        print(os.listdir(folder_dir))
-        shutil.move(folder_dir, tbd)
-        print(os.listdir(tbd))
-        print(os.listdir(folder_dir))
+        if not os.path.exists(f"{home_dir}/tbd"):
+            dummy_dir = "tbd"
+            tbd = os.path.join(os.path.expanduser("~/"), dummy_dir)
+            os.mkdir(tbd)
+        else:
+            print(folder_dir)
+            tbd = f"{home_dir}tbd"
+        for file in folder_contents:
+            print(f"Deleted: {bold + red + file + reset}")
+            shutil.move(f"{home_dir}/Downloads/{file}", tbd)
         shutil.rmtree(tbd)
         if not tbd:
             print("Operation successful")
             break
         else:
             print("Operation unsuccessful")
+            break
+    elif choice == 3:
+        print("It's best we wait...")
         break
     else:
-        print("Invalid entry")
+        print("invalid entry")
+        break
+    print(tbd)
+    print(f"{folder_contents}")
